458f5a931916073348a40a24184e7ff78a491d63
Vidar Rehn <vidar.rehn@gmail.com>
2022-01-17 15:02:29 +0100

    added win/draw message

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ let startButton = document.querySelector(".start-btn");
 let boxes = document.querySelectorAll(".box");
 let playingBoard = document.querySelector(".board");
 let playerTurnDisplay = document.querySelector(".player-turn");
-let circle = '<i class="far fa-circle" value="circle"></i>';
+let victoryMessage = document.querySelector(".victory-msg");
+let circle = '<i class="far fa-circle" value="circle" aria-hidden="true"></i>';
 let cross = '<i class="fas fa-times" value="cross"></i>';
+let winner;
 let win = false;
 let playerTurn;
 let myWinChecker;
 
 
 startButton.addEventListener("click", ()=>{
+    victoryMessage.classList.add("hide");
+    victoryMessage.innerText = "";
     myWinChecker = setInterval(checkWin, 100);
     gameActive = true;
     playerTurn = 1;
@@ -20,7 +24,7 @@ startButton.addEventListener("click", ()=>{
     playingBoard.classList.remove("hide");
     playerTurnDisplay.innerText = `Player ${playerTurn}, it's your turn`;
     boxes.forEach((box)=>{
-        if (gameActive === true){
+        if (gameActive == true){
             if (win == false){
                 box.addEventListener("click", ()=>{
                     if (box.innerHTML == ""){ //checking to see if box is empty
@@ -42,22 +46,63 @@ startButton.addEventListener("click", ()=>{
 
 function checkWin(){
     if ((boxes[0].innerHTML != "" && boxes[1].innerHTML != "" && boxes[2].innerHTML != "") && (boxes[0].innerHTML == boxes[1].innerHTML && boxes[0].innerHTML == boxes[2].innerHTML)){
+        if (boxes[0].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart();
     } else if ((boxes[0].innerHTML != "" && boxes[3].innerHTML != "" && boxes[6].innerHTML != "") && (boxes[0].innerHTML == boxes[3].innerHTML && boxes[0].innerHTML == boxes[6].innerHTML)){
+        if (boxes[0].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[0].innerHTML != "" && boxes[4].innerHTML != "" && boxes[8].innerHTML != "") && (boxes[0].innerHTML == boxes[4].innerHTML && boxes[0].innerHTML == boxes[8].innerHTML)){
+        if (boxes[0].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[1].innerHTML != "" && boxes[4].innerHTML != "" && boxes[7].innerHTML != "") && (boxes[1].innerHTML == boxes[4].innerHTML && boxes[1].innerHTML == boxes[7].innerHTML)){
+        if (boxes[1].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[2].innerHTML != "" && boxes[4].innerHTML != "" && boxes[6].innerHTML != "") && (boxes[2].innerHTML == boxes[4].innerHTML && boxes[2].innerHTML == boxes[6].innerHTML)){
+        if (boxes[2].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[2].innerHTML != "" && boxes[5].innerHTML != "" && boxes[8].innerHTML != "") && (boxes[2].innerHTML == boxes[5].innerHTML && boxes[2].innerHTML == boxes[8].innerHTML)){
+        if (boxes[2].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[3].innerHTML != "" && boxes[4].innerHTML != "" && boxes[5].innerHTML != "") && (boxes[3].innerHTML == boxes[4].innerHTML && boxes[3].innerHTML == boxes[5].innerHTML)){
+        if (boxes[3].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if ((boxes[6].innerHTML != "" && boxes[7].innerHTML != "" && boxes[8].innerHTML != "") && (boxes[6].innerHTML == boxes[7].innerHTML && boxes[6].innerHTML == boxes[8].innerHTML)){
+        if (boxes[6].innerHTML == circle){
+            winner = "Circles";
+        } else {
+            winner = "Crosses";
+        }
         clearAndRestart()
     } else if (boxes[0].innerHTML != "" && boxes[1].innerHTML != "" && boxes[2].innerHTML != "" && boxes[3].innerHTML != "" && boxes[4].innerHTML != "" && boxes[5].innerHTML != "" && boxes[6].innerHTML != "" && boxes[7].innerHTML != "" && boxes[8].innerHTML != "") {
+        winner = "Nobody";
         clearAndRestart()
     }
 }
@@ -72,4 +117,6 @@ function clearAndRestart(){
     playerTurnDisplay.classList.add("hide");
     startButton.classList.remove("hide");
     playingBoard.classList.add("hide");
+    victoryMessage.classList.remove("hide");
+    victoryMessage.innerText = `${winner} won!`;
 }
